180591d56071a924822b805c34926fc235cc0067
Carchedi <carchedi.ufjf@gmail.com>
2025-10-28 20:46:17 -0300

    feat: Configura base URL para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 
 // Configurações do servidor Vite
 export default defineConfig({
+  base: "/landing-encomendas/",
   server: {
     host: true, // permite acesso externo
     port: 5173, // porta padrão do Vite
