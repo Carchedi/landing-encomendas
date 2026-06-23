741d222ca82eb1fd5c0aac57b978359dea3f9bfb
Carchedi <carchedi.ufjf@gmail.com>
2026-06-23 14:07:54 -0300

    Deploy: atualizando com o build do Vite

--- src/js/animations.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('scroll', () => {
-  const elements = document.querySelectorAll('.fade-in');
-  elements.forEach(el => {
-    if(el.getBoundingClientRect().top < window.innerHeight - 100){
-      el.classList.add('visible');
-    }
-  });
-});
